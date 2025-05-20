71e7aa14bcd11ba5e991f2d2ae2cc9a904b5f486
Pedro Lucas <pedrolucas.marlier@gmail.com>
2025-05-20 20:03:20 -0300

    terceiro commit

--- atividade 20-05/No.java
+++ atividade 20-05/No.java
@@ -13,5 +13,14 @@ class No {
         else
             return 1 + contarNos(contar.esquerda) + contarNos(contar.direita);
     }
+
+    public void preOrdem(No pre)
+    {
+        if (pre != null) {
+            System.out.print(pre.valor + " ");
+            preOrdem(pre.esquerda);
+            preOrdem(pre.direita);
+        }
+    }
 }
   
